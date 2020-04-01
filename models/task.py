cb2458cbf01cb342b0926eee5b8e59cc200757e8
maocat <zk246422@163.com>
2020-04-01 17:31:42 +0800

    fix

---
+++ models/task.py
@@ -0,0 +1,40 @@
+from config import mongo
+from mongoengine import SequenceField
+from mongoengine import StringField
+from mongoengine import BooleanField
+from mongoengine import IntField
+from mongoengine import FloatField
+from mongoengine import ListField
+from mongoengine import DateTimeField
+from mongoengine import DENY
+from mongoengine import ReferenceField
+from .users import User
+from werkzeug.security import check_password_hash
+from werkzeug.security import generate_password_hash
+import datetime
+__all__ = [
+
+]
+
+
+class Task(mongo.Document):
+    meta = {'allow_inheritance': True, 'collection': 'task'}
+    create_time = DateTimeField(default=datetime.datetime.now)
+    alias_id = SequenceField()
+
+
+class CustomerTask(Task):
+    user = ReferenceField(User, reverse_delete_rule=DENY)
+    # 图集
+    images = ListField(StringField())
+    # 介绍
+    desc = StringField()
+    # 备注
+    remarks = StringField()
+    # 状态
+    # 0 = 发布中
+    # 1 = 进行中
+    # 2 = 已完成
+    # -1 = 已取消
+    status = IntField(choices=(0, 1, 2, -1), default=0)
+    join_user = ReferenceField(User, reverse_delete_rule=DENY)
